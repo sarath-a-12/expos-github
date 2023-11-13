08d1d5ab7ce6489e64062fbdd94bf914fece5a4e
sarath <112101057@smail.iitpkd.ac.in>
2023-11-13 18:53:19 +0530

    Init

---
+++ stage22.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/zsh
+
+cd $HOME/Documents/myexpos/spl
+echo "Compiling SPL FILES \n"
+for i in $HOME/Documents/sem5_labs/myexpos/stage22/*.spl
+do
+    echo $i
+    # pwd;
+    ./spl $i
+done
+
+
+cd $HOME/Documents/myexpos/expl
+echo ""
+echo "Compiling EXPL FILES\n"
+
+
+for i in $HOME/Documents/sem5_labs/myexpos/stage22/*.expl
+do
+    echo $i
+    ./expl $i
+done
